27dfcdb919ed16bb120476707dc4e7cd9ad2e5b7
bigperson <daein7076@naver.com>
2022-07-19 21:08:28 +0900

    using const, let, var

--- js/app.js
+++ js/app.js
@@ -1,8 +1,16 @@
+// const -> default
+// let   -> sometimes
+// var   -> never use
+
 const a = 10;
 const b = 2;
-const myName = 'bigPerson';
+let myName = 'bigPerson';
 
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
 console.log('hello ' + myName);
+
+myName = 'superBigPerson';
+
+console.log('your new name is ' + myName);
