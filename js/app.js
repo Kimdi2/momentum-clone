be5774ee3f4720b171da2c1fb83b01e884cc3e38
bigperson <daein7076@naver.com>
2022-07-21 03:57:52 +0900

    and, or(2.15)

--- js/app.js
+++ js/app.js
@@ -1,10 +1,25 @@
 //prompt is old function
 const age = parseInt(prompt("How old are you?"));
 
-// console.log(isNaN(age));
+// AND
+// true && true === true
+// true && false === false
+// false && true === false
+// false && false === false
+// OR
+// true || true === true
+// true || false === true
+// false || true === true
+// false || false === false
 
-if (isNaN(age)) {
-  console.log("Please write a number");
-} else {
-  console.log("Thank you for writing a number");
+if (isNaN(age) || age < 0) {
+  console.log("Please write a real positive number");
+} else if (age < 18) {
+  console.log("You are too young to drink");
+} else if (age >= 18 && age <= 50) {
+  console.log("You can drink");
+} else if (age > 50 && age <= 80) {
+  console.log("You should exercise.");
+} else if (age > 80) {
+  console.log("You can do whatever you want");
 }
