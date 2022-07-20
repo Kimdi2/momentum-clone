5bf38b1002434832fe08a5c5c4d512cdb4b54397
bigperson <daein7076@naver.com>
2022-07-21 03:10:01 +0900

    calculator+return(2.11)

--- js/app.js
+++ js/app.js
@@ -1,22 +1,28 @@
 const calculator = {
   add: function (x, y) {
-    console.log(x + y);
+    return x + y;
   },
   minus: function (x, y) {
-    console.log(x - y);
+    return x - y;
   },
   times: function (x, y) {
-    console.log(x * y);
+    return x * y;
   },
   divide: function (x, y) {
-    console.log(x / y);
+    return x / y;
   },
   powerOf: function (x, y) {
-    console.log(x ** y);
+    return x ** y;
   },
 };
-calculator.add(5, 1);
-calculator.minus(3, 9);
-calculator.times(7, 4);
-calculator.divide(5, 2);
-calculator.powerOf(5, 3);
+const plusResults = calculator.add(5, 1);
+const minusResults = calculator.minus(plusResults, 9);
+const timesResults = calculator.times(minusResults, 4);
+const divideResults = calculator.divide(timesResults, 2);
+const powerOfResults = calculator.powerOf(divideResults, 2);
+
+console.log(plusResults);
+console.log(minusResults);
+console.log(timesResults);
+console.log(divideResults);
+console.log(powerOfResults);
