98e83bddb3f491d4ebf4b5207d9bb426a50ceda6
bigperson <daein7076@naver.com>
2022-07-21 18:06:29 +0900

    add, remove class(3.7 ~ 3.8)

--- js/app.js
+++ js/app.js
@@ -1,43 +1,12 @@
 const h1 = document.querySelector(".hello h1"); //like css form
 
 function handleH1Click() {
-  if (h1.style.color !== "blue") {
-    h1.style.color = "blue";
+  const clickedClass = "clicked";
+  if (h1.classList.contains(clickedClass)) {
+    h1.classList.remove(clickedClass);
   } else {
-    h1.style.color = "black";
+    h1.classList.add(clickedClass);
   }
-  console.log(h1.style.color);
-}
-
-function handleMouseEnter() {
-  h1.innerText = "mouse is here";
-}
-
-function handleMouseLeave() {
-  h1.innerText = "mouse is gone";
-}
-
-function handleWindowResize() {
-  document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy() {
-  alert("copier!");
-}
-
-function handleWindowOffline() {
-  alert("SOS no WIFI!");
-}
-
-function handleWindowOnline() {
-  alert("ALL GOOOOOOD");
 }
 
 h1.addEventListener("click", handleH1Click);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
