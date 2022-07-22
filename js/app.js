7c27d9663d02610194eaad7397220c44000c1d1d
bigperson <daein7076@naver.com>
2022-07-22 21:16:37 +0900

    function x(event), (4.3)

--- js/app.js
+++ js/app.js
@@ -1,9 +1,17 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
-function onLoginSubmit(info) {
-  info.preventDefault();
-  console.log(info);
+const link = document.querySelector("a");
+
+function onLoginSubmit(event) {
+  event.preventDefault(); // 기본적인 디폴트값을 막아주는 function ex)submit시 새로고침 방지
+  console.log(loginInput.value);
+}
+
+function handleLinkClick(event) {
+  event.preventDefault();
+  console.dir(event);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
