f75a12a64fe8e4bf3682e80dd213e9135ebd301d
bigperson <daein7076@naver.com>
2022-07-23 20:43:27 +0900

    (5.1)timeout and dates

--- js/clock.js
+++ js/clock.js
@@ -1,7 +1,11 @@
 const clock = document.querySelector("h2#clock");
 
-function sayHello() {
-  console.log("hello");
+function getClock() {
+  const date = new Date();
+  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 }
 
-setInterval(sayHello, 5000);
+// setInterval(function, 5000); // 두번째 인자(5000ms)마다 function 호출
+// setTimeout(function, 5000); // 두번째 인자(5000ms) 후 function 한번 호출
+getClock(); //setInterval 에서 1초 뒤에 실행되기 때문에 처음 실행을 보여주기 위한 함수 실행
+setInterval(getClock, 1000);
