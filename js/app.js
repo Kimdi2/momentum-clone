0a88ffb4566c2ed9050abbd082b55dcd16618ec6
bigperson <daein7076@naver.com>
2022-07-23 19:57:07 +0900

    another loading username(4.7)

--- js/app.js
+++ js/app.js
@@ -8,12 +8,12 @@ const USERNAME_KEY = "username";
 function onLoginSubmit(event) {
   event.preventDefault(); // 기본적인 디폴트값을 막아주는 function ex)submit시 새로고침 방지
   loginForm.classList.add(HIDDEN_CLASSNAME);
-  const username = loginInput.value;
-  localStorage.setItem(USERNAME_KEY, username);
-  paintGreetings(username);
+  localStorage.setItem(USERNAME_KEY, loginInput.value);
+  paintGreetings();
 }
 
-function paintGreetings(username) {
+function paintGreetings() {
+  const username = localStorage.getItem(USERNAME_KEY);
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
@@ -24,5 +24,5 @@ if (savedUsername === null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
   loginForm.addEventListener("submit", onLoginSubmit);
 } else {
-  paintGreetings(savedUsername);
+  paintGreetings();
 }
