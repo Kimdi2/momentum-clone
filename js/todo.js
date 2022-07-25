bcb8dda255d4099cd8747c01db17a54ba263c470
bigperson <daein7076@naver.com>
2022-07-25 20:44:38 +0900

    (7.4 ~ 7.5) loading to dos

--- js/todo.js
+++ js/todo.js
@@ -2,14 +2,17 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
-const toDos = [];
+const TODOS_KEY = "toDos";
+
+let toDos = [];
 
 function saveToDos() {
   /*
   localStorage 는 텍스트만 저장할 수 있음.  
-  JSON.stringify() -> array, object, 어떤 js코드건 간에 string으로 변경해줌  
+  JSON.stringify() -> array, object, 어떤 js코드건 간에 string으로 변경해줌
+  JSON.parse() -> string 을 array, object 등으로 다시 바꿔줌(stringify와 반대 개념)
   */
-  localStorage.setItem("toDos", JSON.stringify(toDos));
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
 function deleteToDo(event) {
@@ -39,3 +42,13 @@ function handleToDoSubmit(event) {
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+// arrow function: (argument) => function 내용;
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+console.log(savedToDos);
+if (savedToDos) {
+  const parsedToDos = JSON.parse(savedToDos);
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintToDo);
+}
