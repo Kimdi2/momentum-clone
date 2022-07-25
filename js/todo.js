3d654891202e08217b20bc6d0e5c02adf1f2925c
bigperson <daein7076@naver.com>
2022-07-25 20:12:42 +0900

    (7.3) saving to dos

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,16 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+const toDos = [];
+
+function saveToDos() {
+  /*
+  localStorage 는 텍스트만 저장할 수 있음.  
+  JSON.stringify() -> array, object, 어떤 js코드건 간에 string으로 변경해줌  
+  */
+  localStorage.setItem("toDos", JSON.stringify(toDos));
+}
+
 function deleteToDo(event) {
   const li = event.target.parentElement;
   li.remove();
@@ -23,7 +33,9 @@ function handleToDoSubmit(event) {
   event.preventDefault();
   const newToDo = toDoInput.value;
   toDoInput.value = "";
+  toDos.push(newToDo);
   paintToDo(newToDo);
+  saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
