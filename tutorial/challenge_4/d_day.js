f8abfc8fd48bc1b216ce60a9b92d672ec9671b85
bigperson <daein7076@naver.com>
2022-07-26 18:04:04 +0900

    code_challenge_4 (ver.2)

--- tutorial/challenge_4/d_day.js
+++ tutorial/challenge_4/d_day.js
@@ -1,18 +1,14 @@
 const dDayWalker = document.querySelector("h2");
 
 function getDDay() {
-  const christmas = new Date(2022, 11, 25) / 1000;
-  const date = Date.now() / 1000;
+  const christmas = new Date(2022, 11, 25) / 1000; // ms -> s
+  const date = Date.now() / 1000; // ms -> s
   const dDay = christmas - date;
 
   const days = String(Math.floor(dDay / 60 / 60 / 24)).padStart(2, "0");
-  const hours = String(Math.floor(dDay / 60 / 60 - days * 24)).padStart(2, "0");
-  const minutes = String(
-    Math.floor(dDay / 60 - days * 24 * 60 - hours * 60)
-  ).padStart(2, "0");
-  const seconds = String(
-    Math.floor(dDay - days * 24 * 60 * 60 - hours * 60 * 60 - minutes * 60)
-  ).padStart(2, "0");
+  const hours = String(Math.floor((dDay / 60 / 60) % 24)).padStart(2, "0");
+  const minutes = String(Math.floor((dDay / 60) % 60)).padStart(2, "0");
+  const seconds = String(Math.floor(dDay % 60)).padStart(2, "0");
   dDayWalker.innerText = `${days}d ${hours}h ${minutes}m ${seconds}s`;
 }
 
